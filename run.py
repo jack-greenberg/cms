5d9e41a3bbbf3c77bb5d8f7075a3adca11168603
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-04-20 10:55:24 -0700

    Add configuration to run.py

--- run.py
+++ run.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+import click
+import sys
 import config
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./site/static/build', template_folder='./site/templates')
+
+@click.command()
+@click.option('--mode', '-m', default='development', help='Production mode (production, development)', required=True)
+def run(mode):
+    if (mode == 'development'):
+        app.config.from_object('config.DevelopmentConfig')
+    elif (mode == 'production'):
+        app.config.from_object('config.ProductionConfig')
+    else:
+        click.echo("Please use either development or production mode.")
+        sys.exit()
+    app.run()
+
 
 @app.route('/')
 def index():
-    return 'Hello to the World of Flask!'
+    return render_template('home.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
 
 if __name__ == '__main__':
-    app.config.from_object('config.DevelopmentConfig')
-    app.run()
+    run()
