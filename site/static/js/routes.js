e760a4aeb5a90075b1f7531761e7bc1bfdcba36f
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-04-25 15:38:02 -0700

    Remove cms.js

--- site/static/js/routes.js
+++ site/static/js/routes.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter, StaticRouter, Router, Route } from 'react-router-dom';
 import { Home } from './components/home';
-import { CMS } from './components/cms';
 
 export default (
     <BrowserRouter>
         <div>
             <Route exact path='/' component={ Home } />
-            <Route path='/admin/' component={ CMS } />
         </div>
     </BrowserRouter>
 )
