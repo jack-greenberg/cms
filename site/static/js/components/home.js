ae862058a632e4a5a149a1a7f915710e84776d22
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-04-25 15:37:41 -0700

    Add axios call to get page list

--- site/static/js/components/home.js
+++ site/static/js/components/home.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const axios = require('axios');
 
 export class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            moduleData: {},
+            isLoaded: false,
+            error: null
+        };
+    };
+
+    componentDidMount() {
+        axios.post('/module-list', {
+            test: "hello"
+        })
+        .then(function(response) {
+            console.log(response["data"]);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+    }
+
     render() {
+
+
         return (
             <h1>Home</h1>
         );
