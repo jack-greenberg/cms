711452b127fd0f49a8ffeba6ec802d42a8505da1
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-06-13 21:59:20 -0700

    Add axios calls to api and link up header-page-title

--- static/backend/js/header.js
+++ static/backend/js/header.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { Navigation } from './nav.js';
 
 export class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            siteTitle: this.props.siteTitle,
+        }
+    }
+    static getDerivedStateFromProps(props, currentState) {
+        return props;
+    }
     render() {
         return (
             <header className="header">
-                <a href="/admin" className="header__title">My Cool Site</a>
+                <a href="/admin" className="header__title">{this.state.siteTitle}</a>
                 <Navigation />
             </header>
         )
--- static/backend/js/home.js
+++ static/backend/js/home.js
@@ -1,29 +1,59 @@
 import React from 'react';
 import { Header } from './header.js';
 import { Footer } from './footer.js';
+import { TextInput } from './components.js';
+import axios from 'axios';
 
 export class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            backendData: undefined,
+        };
+    };
+    componentDidMount() {
+        axios.post('/api/backend-data/')
+            .then(function(response) {
+                this.setState({
+                    backendData: response.data,
+                });
+            }.bind(this))
+            .catch(function(error) {
+                throw new Error(error);
+                console.error(error);
+            });
+    }
     render() {
-        return (
-            <>
-                <Header />
-                <main className="main">
-                    <div className="breadcrumbs">
-                        <h1>Admin / Home</h1>
-                    </div>
-                    <General />
-                    <SEO />
-                </main>
-                <Footer />
-            </>
-        );
+        if (this.state.backendData) {
+            return (
+                <>
+                    <Header siteTitle={this.state.backendData['general']['data']['site-title']}/>
+                    <main className="main">
+                        <div className="breadcrumbs">
+                            <h1>Admin / Home</h1>
+                        </div>
+                        <General inputData={this.state.backendData['general']['data']} />
+                        <SEO inputData={this.state.backendData['seo']['data']} />
+                    </main>
+                    <Footer />
+                </>
+            );
+        } else {
+            return null;
+        };
     };
 };
 
 class General extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            inputData: this.props.inputData,
+        };
     }
+
     render() {
         return (
             <section className="section  section--general">
@@ -33,22 +63,10 @@ class General extends React.Component {
 
                 <div className="section__body">
                     <fieldset className="form-container">
-                        <div className="input-container">
-                            <label htmlFor="form-general--site-title" className="input__label">Site Title</label>
-                            <input type="text" className="input--text  input--text--short  input--important" id="form-general--site-title"/>
-                        </div>
-                        <div className="input-container">
-                            <label htmlFor="form-general--author" className="input__label">Author / Organization</label>
-                            <input type="text" className="input--text  input--text--short" id="form-general--author"/>
-                        </div>
-                        <div className="input-container">
-                            <label htmlFor="form-general--url" className="input__label">URL</label>
-                            <input type="text" className="input--text  input--text--short" id="form-general--url"/>
-                        </div>
-                        <div className="input-container">
-                            <label htmlFor="form-general--google-analytics" className="input__label">Google Analytics Tracking Code</label>
-                            <input type="text" className="input--text  input--text--short" id="form-general--google-analytics"/>
-                        </div>
+                        <TextInput important storedValue={this.state.inputData['site-title']} form="general" name="site-title" label="Site Title"/>
+                        <TextInput storedValue={this.state.inputData['author']} form="general" name="author" label="Author / Organization"/>
+                        <TextInput storedValue={this.state.inputData['url']} form="general" name="url" label="URL"/>
+                        <TextInput storedValue={this.state.inputData['google-analytics']} form="general" name="google-analytics" label="Google Analytics Tracking Code"/>
                     </fieldset>
                 </div>
             </section>
@@ -59,7 +77,27 @@ class General extends React.Component {
 class SEO extends React.Component {
     constructor(props) {
         super(props);
+        this.handleFileInput = this.handleFileInput.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+
+        this.state = {
+            inputData: this.props.inputData,
+        };
     }
+    handleFileInput(e) {
+        let fileName = e.target.value.split('\\').pop();
+        let inputDataCopy = this.state.inputData;
+        inputDataCopy[e.target.getAttribute('data-input-field')] = fileName;
+        this.setState({
+            inputData: inputDataCopy,
+        });
+    };
+    handleTextChange(e) {
+        // get the input field
+        // see how it compares to the saved value
+        // if different, change the color of the box shadow, and of the color of the changed input (add a class (--modified, --deleted, --added))
+        // click a lock to prevent changes?
+    };
     render() {
         return (
             <section className="section  section--seo">
@@ -69,37 +107,31 @@ class SEO extends React.Component {
 
                 <div className="section__body">
                     <fieldset className="form-container">
+                        <TextInput multiline storedValue={this.state.inputData['meta-description']} form="seo" name="meta-description" label="Meta Description"/>
                         <div className="input-container">
-                            <label htmlFor="form-seo--meta-description" className="input__label">Meta Description</label>
-                            <input type="text" className="input--" id="form-seo--meta-description"/>
+                            <div className="input__label">Favicon</div>
+                            <input type="file" id="form-seo--favicon" className="input--file" onChange={this.handleFileInput} data-input-field="favicon" />
+                            <label htmlFor="form-seo--favicon" className="align--horizontal">
+                                <div className="input--file__filename">{this.state.inputData['favicon'] ? this.state.inputData['favicon'] : <span className="text--faint">No file saved</span>}</div>
+                                <div className="input--file__button">Choose a file...</div>
+                            </label>
                         </div>
                         <div className="input-container">
-                            <label htmlFor="form-seo--favicon" className="input__label">Favicon</label>
-                            <input type="file" id="form-seo--favicon" className="input--file"/>
-                            <div className="file-interface">
-                                <div className="file-interface__filename"><span className="text--faint">No file selected</span></div>
-                                <div className="file-interface__button">Choose a file...</div>
-                            </div>
+                            <div className="input__label">Meta Image</div>
+                            <input type="file" id="form-seo--meta-image" className="input--file" onChange={this.handleFileInput} data-input-field="favicon"/>
+                            <label htmlFor="form-seo--meta-image" className="align--horizontal">
+                                <div className="input--file__filename">{this.state.inputData['meta-image'] ? this.state.inputData['meta-image'] : <span className="text--faint">No file saved</span>}</div>
+                                <div className="input--file__button">Choose a file...</div>
+                            </label>
                         </div>
+                        <TextInput multiline code storedValue={this.state.inputData['robots']} form="seo" name="robots" label="robots.txt"/>
                         <div className="input-container">
-                            <label htmlFor="form-seo--meta-image" className="input__label">Meta Image</label>
-                            <input type="file" id="form-seo--meta-image" className="input--file"/>
-                            <div className="file-interface">
-                                <div className="file-interface__filename"><span className="text--faint">No file selected</span></div>
-                                <div className="file-interface__button">Choose a file...</div>
-                            </div>
-                        </div>
-                        <div className="input-container">
-                            <label htmlFor="form-seo--robots-txt" className="input__label">robots.txt</label>
-                            <textarea name="" id="" cols="30" rows="4" className="input--text  input--text--long  input--text--multiline" />
-                        </div>
-                        <div className="input-container">
-                            <label htmlFor="form-seo--sitemap" className="input__label">Sitemap</label>
-                            <input type="file" id="form-seo--sitemap" className="input--file"/>
-                            <div className="file-interface">
-                                <div className="file-interface__filename"><span className="text--faint">No file selected</span></div>
-                                <div className="file-interface__button">Choose a file...</div>
-                            </div>
+                            <div className="input__label">Sitemap</div>
+                            <input type="file" id="form-seo--sitemap" className="input--file" onChange={this.handleFileInput} data-input-field="sitemap" />
+                            <label htmlFor="form-seo--sitemap" className="align--horizontal">
+                                <div className="input--file__filename">{this.state.inputData['sitemap'] ? this.state.inputData['sitemap'] : <span className="text--faint">No file saved</span>}</div>
+                                <div className="input--file__button">Choose a file...</div>
+                            </label>
                         </div>
                     </fieldset>
                 </div>
