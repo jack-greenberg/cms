ba780958b9560efbdf7babd71b31e8193f687081
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-06-13 21:56:55 -0700

    Add API and reset default page

--- run.py
+++ run.py
@@ -4,8 +4,11 @@ import click
 import functools
 import sys
 import config
-from modules.page_data import page_data, page_list
+# from modules.page_data import page_data, page_list
 from modules.auth import User, authenticate #, auth_needed
+from modules.database import db
+from bson import json_util
+import json
 
 app = Flask(__name__, static_folder='./static/build')
 
@@ -23,15 +26,21 @@ def run(mode):
 
 @app.route('/')
 def index():
+    return "coming soon"
     return render_template('site/home.j2', page_title="Home", module_data=page_data['Home'])
 
 @app.route('/<page>')
 def subpage(page):
+    return "coming soon"
+    page_list = []
+    for page in db.pages.find():
+        page_list.append(page["name"])
+
     if page in page_list:
         if page == 'home':
             return redirect(url_for('index'))
         else:
-            return render_template('site/home.j2', page_title=page, module_data=page_data[page])
+            return render_template('site/home.j2', page_title=page, module_data=page_list[page])
     else:
         return render_template('404.j2'), 404
 
@@ -44,10 +53,34 @@ def admin_root():
 def admin_sub(page=None):
     return render_template('admin/admin.j2')
 
+
+# API
 @app.route('/api/<request>/', methods=['POST'])
 def response(request):
-    if (request == 'page-data'):
-        return jsonify(page_data)
+    if (request == 'backend-data'):
+        backend_data = {}
+        robot_text = ""
+
+        with app.open_resource('robots.txt') as f:
+            robot_text = f.read()
+
+        for doc in db.siteData.find():
+            backend_data[doc["name"]] = (json.loads(json_util.dumps(doc)));
+
+        backend_data["seo"]["data"]["robots"] = robot_text.decode()
+        return jsonify(backend_data);
+    elif (request == 'page-list'):
+        page_list = []
+
+        for page in db.pages.find():
+            page_list.append(page["name"])
+        return jsonify(page_list)
+    elif (request == 'robots'):
+        response = ""
+        robots = f.open('robots.txt')
+        for line in robots:
+            response += line + '\n'
+        return jsonify(response)
     else:
         return jsonify("No endpoint requested")
 
