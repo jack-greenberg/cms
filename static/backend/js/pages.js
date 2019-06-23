1525af35fc7512530a09285344400df8e81c5e6e
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-06-23 13:27:09 -0700

    Reset /pages/ and /settings/ to blank

--- static/backend/js/pages.js
+++ static/backend/js/pages.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { Header } from './header.js';
+import { Footer } from './footer.js';
+import { Navigation } from './nav.js';
+import { client } from './index.js';
 
 export class Pages extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            test: 'test',
+            backendData: undefined,
         };
+    };
+    componentDidMount() {
+        client.post('/api/get/siteData/')
+            .then(function(response) {
+                this.setState({
+                    backendData: response.data,
+                });
+            }.bind(this))
+            .catch(function(error) {
+                console.log(error);
+            });
     }
 
     render() {
-        return (
-            <>
-                <h1>PAAAAGES</h1>
-                <Header />
-            </>
-        );
+        if (this.state.backendData) {
+            return (
+                <>
+                    <Navigation />
+                    <Header siteTitle={this.state.backendData['general']['data']['site-title']}/>
+                    <main className="main">
+                        <div className="breadcrumbs">
+                            <h1>Admin / Pages</h1>
+                        </div>
+                    </main>
+                    <Footer />
+                </>
+            );
+        } else {
+            return null;
+        };
     };
 };
