eb5c07f20f9f0ce39391b82236017d2ba5db2838
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-06-26 08:12:53 -0700

    Create structure for eventual breadcrumbs

--- static/backend/js/header.js
+++ static/backend/js/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BackendDataContext } from './index.js';
+import { pad } from './posts.js';
 
 export class Header extends React.Component {
     constructor(props) {
@@ -7,17 +8,33 @@ export class Header extends React.Component {
 
         this.state = {
             siteTitle: this.props.siteTitle,
+            // isPostDraft: this.props.isPostDraft, // comes from singlepost.js and informs breadcrumbs prefix `d-` for post drafts
         }
     }
     static getDerivedStateFromProps(props, currentState) {
         return props;
     }
     render() {
+        // let breadcrumbArray = window.location.pathname.slice(1, -1).split("/");
+        //
+        // for (let i = 0; i < breadcrumbArray.length; i++) {
+        //     // Capitalize
+        //     breadcrumbArray[i] = breadcrumbArray[i].charAt(0).toUpperCase() + breadcrumbArray[i].slice(1);
+        //
+        //     if (i == breadcrumbArray.length - 1) {
+        //         // make the last one bold
+        //         breadcrumbArray[i] = <span className={this.state.isPostDraft ? "breadcrumbs__last" : "breadcrumbs__last  draft-prefix"} key={i}>{pad(breadcrumbArray[i], 4)}</span>
+        //     } else {
+        //         breadcrumbArray[i] = <span key={i}>{breadcrumbArray[i]} > </span>
+        //     }
+        // }
+
         return (
             <BackendDataContext.Consumer>
                 {context => (
                     <header className="header">
                         <a href="/admin" className="header__title">{context.general.data['site-title']}</a>
+                        {/*<p className="breadcrumbs">{breadcrumbArray}</p>*/}
                     </header>
                 )}
             </BackendDataContext.Consumer>
