f2a01195526fc2cbda6a6db9d38005809682c8cf
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-07-23 23:49:55 -0700

    Use DO MongoDB server

--- modules/database.py
+++ modules/database.py
@@ -1,9 +1,15 @@
 from pymongo import MongoClient
 
+# client = MongoClient(
+#     "mongodb://localhost:27017/cms",
+#     username="jackg",
+#     password="observe coleman sullivan guam",
+#     authSource="cms"
+# )
 client = MongoClient(
-    "mongodb://localhost:27017/cms",
+    "206.189.209.148:27017",
     username="jackg",
-    password="observe coleman sullivan guam",
-    authSource="cms"
+    password="devon play refers sold",
+    authSource="cms_dev"
 )
-db = client.cms
+db = client.cms_dev
