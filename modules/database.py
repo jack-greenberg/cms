c14487a2b25816da171ba9fb3084acba82120bc2
jack-Greenberg <j.lester.greenberg@gmail.com>
2019-07-25 18:36:04 -0700

    Move password to ignored file and import with yaml

--- modules/database.py
+++ modules/database.py
@@ -1,15 +1,16 @@
 from pymongo import MongoClient
+import sys
+from os.path import dirname, join
+import yaml
 
-# client = MongoClient(
-#     "mongodb://localhost:27017/cms",
-#     username="jackg",
-#     password="observe coleman sullivan guam",
-#     authSource="cms"
-# )
+file = open(join(dirname(__file__), '../pw.yaml'))
+
+db_data = yaml.load(file.read())['production' if true else 'dev']['db']
+
 client = MongoClient(
-    "206.189.209.148:27017",
-    username="cmsUser",
-    password="russell textile classroom does",
-    authSource="cms_production"
+    db_data.host + ':' + db_data.port,
+    username=db_data.user,
+    password=db_data.pw,
+    authSource=db_data.dbName
 )
 db = client.cms_production
