1fabff4398934a410586a1beecef42c07bafe48c
jack-Greenberg <j.lester.greenberg@gmail.com>
2019-08-01 21:18:40 -0700

    Re-add <Tags />

--- static/backend/js/singlepost.js
+++ static/backend/js/singlepost.js
@@ -92,7 +92,7 @@ export class SinglePost extends React.Component {
                             </main>
                             <aside>
                                 {/*<Actions />*/}
-                                {/*<Tags />*/}
+                                <Tags tags={this.state.postData.tags} postId={this.state.postData['_id']['$oid']} />
                             </aside>
                             <Footer />
                         </div>
@@ -191,8 +191,97 @@ class PostEditor extends React.Component {
                         </div>
                     )}
                 </div>
-                <button onClick={this.props.updateEdited} contentid="12345" editedstatus={'true'}>Click me</button>
             </article>
+        )
+    }
+}
+
+class Tags extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleTagKeyUp = this.handleTagKeyUp.bind(this);
+        this.addTag = this.addTag.bind(this);
+        this.removeTag = this.removeTag.bind(this);
+
+        this.state = {
+            tags: this.props.tags,
+        }
+    }
+    handleTagKeyUp(e) {
+        if (e.keyCode == 13 && e.target.value == "") {
+            // user presses enter, but nothing is in the field
+            return;
+        };
+        if (e.keyCode == 13) {
+            this.addTag();
+        } else {
+            return;
+        }
+    }
+    addTag(e) {
+        if ($('.js-add-tag__input').val() == "") {
+            return;
+        }
+        var tagList = this.state.tags;
+        tagList.push($('.js-add-tag__input').val());
+        this.setState({
+            tags: tagList.filter((v, i, a) => a.indexOf(v) === i),
+        }, function() {
+            client.put('/api/v1/posts/' + this.props.postId, {
+                tags: this.state.tags,
+            })
+            .then(response => {
+                console.log(response);
+            })
+        });
+        $('.js-add-tag__input').val("");
+        $('.js-add-tag__input').focus();
+    }
+    removeTag(e) {
+        var tagList = this.state.tags;
+        var tagToRemove = $(e.target).closest('button').attr("value");
+        var newTagList = arrayRemove(tagList, tagToRemove);
+        this.setState({
+            tags: newTagList,
+        }, function() {
+            client.put('/api/v1/posts/' + this.props.postId, {
+                tags: this.state.tags,
+            })
+            .then(response => {
+                console.log(response);
+            })
+        })
+    }
+    render() {
+        return (
+            <section className="section  main__general__tags">
+                <h2 className="section__heading">Tags</h2>
+                <div className="tag-list  flex-wrapper">
+                    {this.state.tags.map((tag, index) => {
+                        return (
+                            <div className="tag" key={index}>
+                                <button onClick={this.removeTag} value={tag}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                        <path d="M9.172 16.242L12 13.414 14.828 16.242 16.242 14.828 13.414 12 16.242 9.172 14.828 7.758 12 10.586 9.172 7.758 7.758 9.172 10.586 12 7.758 14.828z" />
+                                        <path d="M12,22c5.514,0,10-4.486,10-10S17.514,2,12,2S2,6.486,2,12S6.486,22,12,22z M12,4c4.411,0,8,3.589,8,8s-3.589,8-8,8 s-8-3.589-8-8S7.589,4,12,4z" />
+                                    </svg>
+                                </button>
+                                {tag}
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="add-tag">
+                    <input className="add-tag__input  js-add-tag__input" type="text" placeholder="Add a new tag..." onKeyUp={this.handleTagKeyUp}/>
+                    <button className="add-tag__button" onClick={this.addTag}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M13 7L11 7 11 11 7 11 7 13 11 13 11 17 13 17 13 13 17 13 17 11 13 11z"/>
+                            <path d="M12,2C6.486,2,2,6.486,2,12s4.486,10,10,10c5.514,0,10-4.486,10-10S17.514,2,12,2z M12,20c-4.411,0-8-3.589-8-8 s3.589-8,8-8s8,3.589,8,8S16.411,20,12,20z"/>
+                        </svg>
+                    </button>
+                </div>
+            </section>
         )
     }
 }
@@ -282,95 +371,6 @@ class PostEditor extends React.Component {
 //                 </section>
 //                 <Tags tags={this.props.postData['tags']} postId={this.props.postData['_id']['$oid']} />
 //             </article>
-//         )
-//     }
-// }
-// class Tags extends React.Component {
-//     constructor(props) {
-//         super(props);
-//
-//         this.handleTagKeyUp = this.handleTagKeyUp.bind(this);
-//         this.addTag = this.addTag.bind(this);
-//         this.removeTag = this.removeTag.bind(this);
-//
-//         this.state = {
-//             tags: this.props.tags,
-//         }
-//     }
-//     handleTagKeyUp(e) {
-//         if (e.keyCode == 13 && e.target.value == "") {
-//             // user presses enter, but nothing is in the field
-//             return;
-//         };
-//         if (e.keyCode == 13) {
-//             this.addTag();
-//         } else {
-//             return;
-//         }
-//     }
-//     addTag(e) {
-//         if ($('.js-add-tag__input').val() == "") {
-//             return;
-//         }
-//         var tagList = this.state.tags;
-//         tagList.push($('.js-add-tag__input').val());
-//         this.setState({
-//             tags: tagList.filter((v, i, a) => a.indexOf(v) === i),
-//         }, function() {
-//             client.put('/api/v1/posts/' + this.props.postId, {
-//                 tags: this.state.tags,
-//             })
-//             .then(response => {
-//                 console.log(response);
-//             })
-//         });
-//         $('.js-add-tag__input').val("");
-//         $('.js-add-tag__input').focus();
-//     }
-//     removeTag(e) {
-//         var tagList = this.state.tags;
-//         var tagToRemove = $(e.target).closest('button').attr("value");
-//         var newTagList = arrayRemove(tagList, tagToRemove);
-//         this.setState({
-//             tags: newTagList,
-//         }, function() {
-//             client.put('/api/v1/posts/' + this.props.postId, {
-//                 tags: this.state.tags,
-//             })
-//             .then(response => {
-//                 console.log(response);
-//             })
-//         })
-//     }
-//     render() {
-//         return (
-//             <section className="section  main__general__tags">
-//                 <h2 className="section__heading">Tags</h2>
-//                 <div className="tag-list  flex-wrapper">
-//                     {this.state.tags.map((tag, index) => {
-//                         return (
-//                             <div className="tag" key={index}>
-//                                 <button onClick={this.removeTag} value={tag}>
-//                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-//                                         <path d="M9.172 16.242L12 13.414 14.828 16.242 16.242 14.828 13.414 12 16.242 9.172 14.828 7.758 12 10.586 9.172 7.758 7.758 9.172 10.586 12 7.758 14.828z" />
-//                                         <path d="M12,22c5.514,0,10-4.486,10-10S17.514,2,12,2S2,6.486,2,12S6.486,22,12,22z M12,4c4.411,0,8,3.589,8,8s-3.589,8-8,8 s-8-3.589-8-8S7.589,4,12,4z" />
-//                                     </svg>
-//                                 </button>
-//                                 {tag}
-//                             </div>
-//                         )
-//                     })}
-//                 </div>
-//                 <div className="add-tag">
-//                     <input className="add-tag__input  js-add-tag__input" type="text" placeholder="Add a new tag..." onKeyUp={this.handleTagKeyUp}/>
-//                     <button className="add-tag__button" onClick={this.addTag}>
-//                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-//                             <path d="M13 7L11 7 11 11 7 11 7 13 11 13 11 17 13 17 13 13 17 13 17 11 13 11z"/>
-//                             <path d="M12,2C6.486,2,2,6.486,2,12s4.486,10,10,10c5.514,0,10-4.486,10-10S17.514,2,12,2z M12,20c-4.411,0-8-3.589-8-8 s3.589-8,8-8s8,3.589,8,8S16.411,20,12,20z"/>
-//                         </svg>
-//                     </button>
-//                 </div>
-//             </section>
 //         )
 //     }
 // }
