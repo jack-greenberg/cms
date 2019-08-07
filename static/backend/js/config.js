dbf1b20d26add7e487222e1e7137713b21f5ec85
jack-Greenberg <j.lester.greenberg@gmail.com>
2019-08-06 19:41:31 -0700

    Add config js file

---
+++ static/backend/js/config.js
@@ -0,0 +1,28 @@
+const cf;
+
+export default cf = {
+    title: "Contentify",
+    urlPrefix: "admin",
+    nav: [
+        {
+            name: "Home",
+            url: "/home/",
+            // component: <Home />,
+        },
+        {
+            name: "Pages",
+            url: "/pages/",
+            // component: <Home />,
+        },
+        {
+            name: "Posts",
+            url: "/posts/",
+            // component: <Home />,
+        },
+        {
+            name: "Images",
+            url: "/images/",
+            // component: <Home />,
+        },
+    ],
+};
