991ffd4b848549280f4798863a1a87fcaa49006f
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-05-09 15:11:59 -0700

    Delete old files

--- theme/static/js/index.js
+++
@@ -1,6 +0,0 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import AppRouter from './routes.js';
-//
-// ReactDOM.render(<AppRouter />, document.getElementById("content"));
-import '../css/index.scss';
